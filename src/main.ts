316e25f8592fa9f2d45f4bedd06bfa0fe981e2da
王兴家 <xingwangzhe@outlook.com>
2026-02-22 16:56:12 +0800

    fix: 更新导入路径以使用别名，提升代码一致性

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import pinia from "./stores";
-import "./style.css";
+import App from "@/App.vue";
+import router from "@/router";
+import pinia from "@/stores";
+import "@style.css";
 import VueECharts from "vue-echarts";
 import { use } from "echarts/core";
 import { PieChart, LineChart } from "echarts/charts";
