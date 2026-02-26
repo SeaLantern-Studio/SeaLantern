b27b3f44018083f27cd375cfc32ed5a632d07721
CmzYa <mjc755366550@163.com>
2026-02-26 15:53:01 +0800

    refactor(UI): 优化语言菜单样式并简化丙烯酸效果描述
    更新语言菜单的样式以使用CSS变量实现更灵活的视觉效果，同时移除丙烯酸效果不支持时的冗余描述

--- src/components/views/paint/AppearanceCard.vue
+++ src/components/views/paint/AppearanceCard.vue
@@ -140,13 +140,7 @@ function handleMinimalModeChange(value: boolean) {
       <div class="sl-setting-row">
         <div class="sl-setting-info">
           <span class="sl-setting-label">{{ i18n.t("settings.acrylic") }}</span>
-          <span class="sl-setting-desc">
-            {{
-              acrylicSupported
-                ? i18n.t("settings.acrylic_desc")
-                : i18n.t("settings.acrylic_not_supported")
-            }}
-          </span>
+          <span class="sl-setting-desc">{{ i18n.t("settings.acrylic_desc") }}</span>
         </div>
         <SLSwitch
           :model-value="acrylicEnabled"
