00a429badeffd4519fb109d843183f964911e969
Kercy <152250600+KercyDing@users.noreply.github.com>
2026-05-10 18:17:46 +0800

    revert: 添加 SL.json 启动配置的读取和写入功能 (#711)" (#713)
    This reverts commit 3f1900718c516db2a8edd1ea46dc1ca47d0e4dcc.

--- src-tauri/src/runtime/command_catalog.rs
+++ src-tauri/src/runtime/command_catalog.rs
@@ -63,8 +63,6 @@ pub(crate) fn desktop_handler(
         config_commands::parse_server_properties_source,
         config_commands::preview_server_properties_write,
         config_commands::preview_server_properties_write_from_source,
-        config_commands::read_sl_config,
-        config_commands::write_sl_config,
         system_commands::get_system_info,
         system_commands::get_server_resource_usage,
         system_commands::pick_jar_file,
